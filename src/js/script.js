92c07b4b70d970d2a5dd84198fcd9e5a4c51a13e
Michael <example@gmail.com>
2020-09-15 16:49:18 +0800

    second screen half done

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+
     //Адаптация first__bar 
     const bar = document.querySelector('.first__bar'),
           parentBar = document.querySelector('.first__content'),
@@ -12,9 +13,20 @@ window.addEventListener('DOMContentLoaded', () => {
             const parentWidth = getStyleWidth(parentBar);
             bar.style.right = `${(barRightValue + (parentWidth - mobileWidth)/2)}px`;
         });
+    }else if(window.matchMedia('(max-width: 768px)').matches) {
+        bar.style.right = `200px`;
+    }else if(window.matchMedia('(max-width: 992px)').matches) {
+        bar.style.right = `156px`;
+    }else if(window.matchMedia('(max-width: 1200px)').matches) {
+        bar.style.right = `50px`;
+        console.log('here');
+    }else {
+        bar.style.right = 0;
     }
 
     function getStyleWidth(element) {
         return +window.getComputedStyle(element).width.slice(0, -2);
     }
+
+    //Создаем карточки на второй странице
 });
